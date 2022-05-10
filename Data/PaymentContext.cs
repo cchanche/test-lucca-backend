d10876e734ef22e6673c381ca2202665a531931c
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2022-05-10 08:24:02 +0200

    feat(validation): 🚧 WIP perform entity validation

--- Data/PaymentContext.cs
+++ Data/PaymentContext.cs
@@ -9,13 +9,13 @@ namespace Payments.Data
 
     public PaymentContext(DbContextOptions<PaymentContext> options) : base(options)
     {
+      // The following configures EF to create a Sqlite database file in the
+      // special "local" folder for your platform.
       var folder = Environment.SpecialFolder.LocalApplicationData;
       var path = Environment.GetFolderPath(folder);
       DbPath = System.IO.Path.Join(path, "Payments.db");
     }
 
-    // The following configures EF to create a Sqlite database file in the
-    // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite($"Data Source={DbPath}");
 
@@ -29,6 +29,12 @@ namespace Payments.Data
       modelBuilder.Entity<Currency>().ToTable("Courses");
       modelBuilder.Entity<Payment>().ToTable("Payments");
       modelBuilder.Entity<User>().ToTable("Students");
+    }
+
+    // Override SaveChanges to perform entity validation
+    public override int SaveChanges()
+    {
+      return base.SaveChanges(); // base keyword is equivalent to super
     }
   }
 }
